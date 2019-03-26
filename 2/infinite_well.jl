b33fbe4ddab810d87f0017c8b93cf21986139b7d
Bernhard Gstrein <bernhard-gstrein@mailbox.org>
2019-03-26 16:42:05 +0100

    Implement infinite well in julia

--- 2/infinite_well.jl
+++ 2/infinite_well.jl
@@ -2,16 +2,21 @@ using LinearAlgebra
 using PyPlot
 using PyCall
 using Printf
+pygui(false)
 
 L = π
-V0 = 1000
-step = .05
-m = .5
 
-pos_basis = [-L:step:L;]
+Npoints = 32
+basis = range(-L, stop = L, length = Npoints)
+pos_basis = collect(basis)
+step = 2 * L / Npoints
+
 pts = size(pos_basis)[1]
 V = zeros(pts, pts)
 p_sqr = zeros(pts, pts)
+
+V0 = 10000
+m = .1999 / sqrt(step)
 
 # Function for evaluating potential
 function pot(x)
@@ -22,59 +27,71 @@ function pot(x)
     end
 end
 
-# Potential
+# Calculate potential
 for i = 1:pts
     V[i,i] = pot(pos_basis[i])
 end
 
-# Momentum operator
-for i = 1:pts
-    if i < pts
-        p_sqr[i,i] = -2 / step^2
-        p_sqr[i+1,i] = 1 / step^2
-        p_sqr[i,i+1] = 1 / step^2
-    else
-        p_sqr[i,i] = -2 / step^2
-    end
-end
-
-# # Momentum operator more steps
+# # Momentum operator; second derivative
 # for i = 1:pts
-#     if i < pts-2
-#         p_sqr[i,i] = -49/18 / step^2
-#         p_sqr[i+1,i] = 3/2 / step^2
-#         p_sqr[i,i+1] = 3/2 / step^2
-#         p_sqr[i+2,i] = -3/2 / step^2
-#         p_sqr[i,i+2] = -3/2 / step^2
-#         p_sqr[i+3,i] = 1/90 / step^2
-#         p_sqr[i,i+3] = 1/90 / step^2
-#     elseif i < pts-1
-#         p_sqr[i,i] = -49/18 / step^2
-#         p_sqr[i+1,i] = 3/2 / step^2
-#         p_sqr[i,i+1] = 3/2 / step^2
-#         p_sqr[i+2,i] = -3/2 / step^2
-#         p_sqr[i,i+2] = -3/2 / step^2
-#     elseif i < pts
-#         p_sqr[i,i] = -49/18 / step^2
-#         p_sqr[i+1,i] = 3/2 / step^2
-#         p_sqr[i,i+1] = 3/2 / step^2
+#     if i < pts
+#         p_sqr[i,i] = -2 / step^2
+#         p_sqr[i+1,i] = 1 / step^2
+#         p_sqr[i,i+1] = 1 / step^2
 #     else
-#         p_sqr[i,i] = -49/18 / step^2
+#         p_sqr[i,i] = -2 / step^2
 #     end
 # end
 
-H_kin = -p_sqr/2m
-H_kin = H_kin
+# Momentum operator more steps
+for i = 1:pts
+    if i < pts-2
+        p_sqr[i,i] = -49/18 / step^2
+        p_sqr[i+1,i] = 3/2 / step^2
+        p_sqr[i,i+1] = 3/2 / step^2
+        p_sqr[i+2,i] = -3/20 / step^2
+        p_sqr[i,i+2] = -3/20 / step^2
+        p_sqr[i+3,i] = 1/90 / step^2
+        p_sqr[i,i+3] = 1/90 / step^2
+    elseif i < pts-1
+        p_sqr[i,i] = -49/18 / step^2
+        p_sqr[i+1,i] = 3/2 / step^2
+        p_sqr[i,i+1] = 3/2 / step^2
+        p_sqr[i+2,i] = -3/20 / step^2
+        p_sqr[i,i+2] = -3/20 / step^2
+    elseif i < pts
+        p_sqr[i,i] = -49/18 / step^2
+        p_sqr[i+1,i] = 3/2 / step^2
+        p_sqr[i,i+1] = 3/2 / step^2
+    else
+        p_sqr[i,i] = -49/18 / step^2
+    end
+end
 
+# Hamilton operators
+H_kin = -p_sqr/2m / (π^2 / L^2)
+H_kin = H_kin
 H = H_kin + V
 
-
 eigenvalues, eigenvectors = eigen(H)
-println(eigenvalues)
-println("")
 
 clf()
-n = 3
-plot(pos_basis, eigenvectors[:,n])
-suptitle("Wavefunction for n = " *@sprintf("%.0f", n))
+for n in 1:3
+    lbl = "n=$(n)"
+    psi = eigenvalues[n] .+ abs2.(eigenvectors[:,n]./sqrt(step))
+    plot(pos_basis, psi, label=lbl)
+end
+
+xlabel(L"x")
+ylabel(L"|\psi_n(x)|^2")
+title("Infinite well wave function densities hand-engineered")
+legend(loc="upper right", bbox_to_anchor=(.95, .8))
+
 gcf()
+
+println("Points in basis: $(size(pos_basis)[1])")
+println("Energies hand-engineered:")
+for n in 1:3
+    println(@sprintf("%.5f", eigenvalues[n]))
+end
+println("")
---
+++ 2/infinite_well_julia.jl
@@ -0,0 +1,49 @@
+using QuantumOptics
+using PyPlot
+using PyCall
+pygui(false)
+
+# Parameters
+L = π
+Npoints = 64
+V0 = 1000
+m = .5
+
+b_position = PositionBasis(-L, L, Npoints)
+step = 2 * L / Npoints
+
+p = momentum(b_position)
+
+function potential(x)
+    if abs(x) > L/2
+        return V0
+    else
+        return 0
+    end
+end
+
+V = potentialoperator(b_position, potential)
+
+H_kin = p^2/2m / (π^2 / L^2)
+H = H_kin + V
+
+E, states = eigenstates((H + dagger(H))/2, 3);
+
+xpoints = samplepoints(b_position)
+clf()
+for n in 1:3
+    lbl = "n=$(n)"
+    plot(xpoints, E[n] .+ abs2.(states[n].data./sqrt(step)), label=lbl)
+end
+
+xlabel(L"x")
+ylabel(L"|\psi_n(x)|^2")
+title("Infinite well wave function densities in QO Julia")
+legend(loc="upper right", bbox_to_anchor=(.95, .8))
+
+gcf()
+
+# println("Energies QO Julia:")
+# for n in 1:3
+#     println(E[n])
+# end
